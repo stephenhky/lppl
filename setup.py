948db2e2f9a06cf6f9e208c7d7c71ae8236360e0
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-07-29 18:58:17 -0400

    updated pyproject.toml

--- setup.py
+++ setup.py
@@ -2,12 +2,5 @@
 from setuptools import setup
 
 
-def install_requirements():
-    return [package_string.strip() for package_string in open('requirements.txt', 'r')]
-
-
 setup(url="https://github.com/stephenhky/lppl",
-      install_requires=install_requirements(),
-      test_require=['unittest'],
-      test_suite="test",
-      zip_safe=False)
+      test_suite="test")
